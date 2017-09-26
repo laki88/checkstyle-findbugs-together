29c0c9ce6208bf8daded5b3fb1f1b0add2bfa38c
Lakshman Udayakantha <LakshmanU@mitrai.local>
2017-09-26 13:20:24 +0530

    Add minimal project with checkstyle and findbugs

---
+++ src/main/java/com/mitrai/App.java
@@ -0,0 +1,22 @@
+package com.mitrai;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+    private static Log log = LogFactory.getLog(App.class);
+
+    public static void main(String[] args) {
+        // code added to check findbugs.
+//        String test = null;
+//        if (test.equals("")) {
+//            log.info("string is empty.");
+//        }
+       log.info("Hello world...");
+    }
+}
